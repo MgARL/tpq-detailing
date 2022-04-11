a834e1f4d4c1ab08f4c7aa67363afd1c7aa4629b
Miguel(MgARL) <mgarlcr@gmail.com>
2022-04-11 13:11:41 -0400

    Removed Unused vars

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import logo from './tpq-logo.png'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
 
 // Comps
 import MyNavbar from './components/MyNavbar'
